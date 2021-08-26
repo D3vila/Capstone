4f51273fe074c1d5a1333f2f22956bf9b7848bd9
Antonio Davila-Olivares <ajd57@nau.edu>
2021-08-26 00:34:33 -0700

    completed backend location routes

---
+++ app/api/location_route.py
@@ -0,0 +1,16 @@
+from flask import Blueprint
+from app.models import Location
+
+location_route = Blueprint('location', __name__)
+
+
+@location_route.route('/')
+def locations():
+    locations = Location.query.all()
+    return {'locations': [location.to_dict() for location in locations]}
+
+
+@location_route.route('/<int:id>')
+def location(id):
+    location = Location.query.get(id)
+    return {'location': location.to_dict(), 'reviews': [item.to_dict() for item in location.review]}
