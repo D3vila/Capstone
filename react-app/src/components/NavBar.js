9174d9c1b6ab438af965e8cb6d5f380af7094f03
Antonio Davila-Olivares <ajd57@nau.edu>
2021-08-25 22:09:45 -0700

    fixed navbar links with session user

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,36 +1,53 @@
 
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
 
 const NavBar = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
+  const sessionUser = useSelector(state => state.session.user);
+
+  let sessionLinks
+  if (sessionUser) {
+    sessionLinks = (
+      <div className='nav__links'>
+        <NavLink to={`/users/${sessionUser.id}`} exact={true} activeClassName='active' className='user__profile__link'>
+          My Profile
+        </NavLink>
+        <LogoutButton user={sessionUser} className='logout__button' />
+      </div>
+    )
+  } else {
+    sessionLinks = (
+      <div>
+        <div>
+          <NavLink to='/sign-up' exact={true} activeClassName='active' className='signup__link'>
             Sign Up
           </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
+          <NavLink to='/login' exact={true} activeClassName='active' className='login__link'>
+            Login
           </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
+        </div>
+      </div>
+    )
+  }
+
+
+  return (
+    <nav>
+      <div>
+        <NavLink to='/' exact={true} activeClassName='active'>
+          Home
+        </NavLink>
+      </div>
+      <div>
+        <NavLink to='/users' exact={true} activeClassName='active'>
+          Users
+        </NavLink>
+      </div>
+      <div>
+        {sessionLinks}
+      </div>
     </nav>
   );
 }
